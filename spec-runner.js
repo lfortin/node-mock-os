998ca0b1fd056a51f7e3b5c0c47dbb48c4e2877e
Laurent Fortin <laurent.fortin@gmail.com>
2015-08-15 10:54:03 -0400

    more tests

--- spec-runner.js
+++ spec-runner.js
@@ -2,12 +2,13 @@
 
 var assert = require('assert'),
     os = require('os'),
-    mock = require('./mock-os');
+    mock = require('./mock-os'),
+    EOL = os.EOL;
 
 function getEOL(n) {
   var lines = [];
   for(var i = 0; i < n; i++) {
-    lines.push(os.EOL);
+    lines.push(EOL);
   }
   return lines.join('');
 }
@@ -24,7 +25,29 @@ assert.doesNotThrow(function() {
 
   process.stdout.write("API signature OK" + getEOL(1));
 
-  // TODO: more tests
+  // fake system metrics
+  mock({
+    freemem: 100,
+    loadavg: [1, 2, 3],
+    EOL: 'TEST'
+  });
+  
+  assert.deepEqual(os.freemem(), 100, ".freemem() expected to return 100");
+  assert.deepEqual(os.loadavg()[0], 1, ".loadavg() expected to return [1, 2, 3]");
+  assert.deepEqual(os.loadavg()[1], 2, ".loadavg() expected to return [1, 2, 3]");
+  assert.deepEqual(os.loadavg()[2], 3, ".loadavg() expected to return [1, 2, 3]");
+  assert.deepEqual(os.EOL, 'TEST', "EOL expected to be === 'TEST'");
 
+  process.stdout.write("mock() OK" + getEOL(1));
+  
+  mock.restore();
+  
+  assert.notDeepEqual(os.freemem(), 100, ".freemem() expected to not return 100");
+  assert.notDeepEqual(os.loadavg()[0], 1, ".loadavg() expected to not return [1, 2, 3]");
+  assert.notDeepEqual(os.loadavg()[1], 2, ".loadavg() expected to not return [1, 2, 3]");
+  assert.notDeepEqual(os.loadavg()[2], 3, ".loadavg() expected to not return [1, 2, 3]");
+  assert.notDeepEqual(os.EOL, 'TEST', "EOL expected to be !== 'TEST'");
+  
+  process.stdout.write("mock.restore() OK" + getEOL(1));
 });
 
